2a6d947cd6b3597ac980f96ab51b6dc6f1437a5b
Matheus Gomes <mth.gom@gmail.com>
2021-12-18 15:13:35 -0300

    Relationship in models

--- src/models/appointment.ts
+++ src/models/appointment.ts
@@ -4,15 +4,22 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
 
+import User from './user';
 @Entity('appointments') //nome da tabela
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
