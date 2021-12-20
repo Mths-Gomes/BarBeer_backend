b71c1dfe072e99a2a30db46fae0f2defca752295
Matheus Gomes <mth.gom@gmail.com>
2021-12-20 11:14:32 -0300

    Add Avatar JPG

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { request, Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/createUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 interface UserWithoutPassword {
   name: string;
@@ -31,5 +35,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
